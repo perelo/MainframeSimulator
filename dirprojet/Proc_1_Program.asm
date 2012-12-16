e4f642d3d0cc8a191c8c03fedc355034dfa71bf1
Eloi Perdereau <eloi@perdereau.eu>
2012-12-16 17:00:21 +0100

    modify Proc_1&2 to make producer-consumer : write 1, ... 10 in shm & read + output

--- dirprojet/Proc_1_Program.asm
+++ dirprojet/Proc_1_Program.asm
@@ -1,27 +1,35 @@
-SETRI R0 0        ; index of first (and unique) semaphore 
-SETRI R2 1        ; code of P(), and also count of semop()s
-SETRI R3 210      ; address for the P() semop
-STMEM R3 R0       ; store the index of the semaphore to operate on
+SETRI R0 0        ; index of semRead semaphore
+SETRI R2 1        ; code of P(), also count of semop()s, and also index of semWrite
+SETRI R3 215      ; address for the P() semop for semRead
+STMEM R3 R0       ; store the index of the semaphore to operate on (semRead)
 ADDRG R3 R3 R2    ; advance the address
 STMEM R3 R2       ; store the P() operation code
-SETRI R3 220      ; address for the V() semop
-STMEM R3 R0       ; store the index of the semaphore to operate on
+SETRI R3 220      ; address for the V() semop on semWrite
+STMEM R3 R2       ; store the index of the semaphore to operate on (semWrite)
 ADDRG R3 R3 R2    ; advance the address
 STMEM R3 R0       ; store the V() operation code
-SETRI R5 10       ; shared memory address (agreed upon with the other proc)
 SETRI R4 4        ; int number for semop() request, that is int4 
-SETRI R3 210      ; waitLoop , set address for the P() semop
-CLINT R4          ; P() the semaphore
-LDSHM R5 R6       ; read the shared memory value
-SETRI R3 220      ; set address for the V() semop
-CLINT R4          ; V() the semaphore
-JZROI R6 -5       ; jump back to waitLoop if the value read from shmem is zero
-SETRI R3 300      ; some address in process memory
-STMEM R3 R6       ; store the value read from shared memory at that address in proc mem
-SETRI R4 380      ; some other address in process memory, for the "format" 
-SETRI R1 1        ; type char
-STMEM R4 R1       ; store the type value
-SETRI R2 1        ; how many elements -- just one
+SETRI R3 215      ; set address for the P() semop on semRead
+CLINT R4          ; P() the semaphore semRead for initialization
+SETRI R5 11       ; shared memory start address (agreed upon with the other proc)
+SETRI R7 10       ; counter : number of values left to read
+SETRI R8 300      ; address where to write the values read
+SETRI R9 380      ; address where to write the value types (int)
+SETRI R3 215      ; waitLoop , set address for the P() semop on semRead
+CLINT R4          ; P() the semaphore semRead
+LDSHM R5 R6       ; read from the shared memory value
+SETRI R3 220      ; set address for the V() semop on semWite
+CLINT R4          ; V() the semaphore semWrite
+STMEM R8 R6       ; write the value we just read in the vect to be output
+STMEM R9 R0       ; write the value type (type int)
+ADDRG R8 R8 R2    ; increment the address where to write the values to read
+ADDRG R9 R9 R2    ; increment the address where to write the value types (int)
+ADDRG R5 R5 R2    ; increment the address where to read from the shared memory
+SUBRG R7 R7 R2    ; decrement counter
+JNZRI R7 -11      ; jump to read another value while counter isn't at 0
+SETRI R3 300      ; start address in process memory where we've written the read values
+SETRI R4 380      ; start address in process memory, for the "format"
+SETRI R2 10       ; how many elements -- 10
 SETRI R0 5        ; int number for consoleOut request
 CLINT R0          ; call interruption int5 for kernel consoleOut
 SETRI R9 2        ; int number for exit()
--- dirprojet/Proc_2_Program.asm
+++ dirprojet/Proc_2_Program.asm
@@ -1,20 +1,25 @@
-SETRI R0 0        ; index of first (and unique) semaphore 
-SETRI R2 1        ; code of P(), and also count of semop()s
-SETRI R3 215      ; address for the P() semop
-STMEM R3 R0       ; store the index of the semaphore to operate on
+SETRI R0 0        ; kernel pid, and also index of semRead
+SETRI R2 1        ; code of P(), also count of semop()s, and also index of semWrite
+SETRI R3 225      ; address for the P() semop on semWrite
+STMEM R3 R2       ; store the index of the semaphore to operate on (semWrite)
 ADDRG R3 R3 R2    ; advance the address
 STMEM R3 R2       ; store the P() operation code
-SETRI R3 225      ; address for the V() semop
-STMEM R3 R0       ; store the index of the semaphore to operate on
+SETRI R3 210      ; address for the V() semop on semRead
+STMEM R3 R0       ; store the index of the semaphore to operate on (semRead)
 ADDRG R3 R3 R2    ; advance the address
 STMEM R3 R0       ; store the V() operation code
-SETRI R5 10       ; shared memory address (agreed upon with the other proc)
+SETRI R5 11       ; shared memory start address (agreed upon with the other proc)
 SETRI R4 4        ; int number for semop() request, that is int4
-SETRI R6 65       ; value to write in shared memory
-SETRI R3 215      ; set address for the P() semop
-CLINT R4          ; P() the semaphore
+SETRI R6 1        ; first value to write in shared memory
+SETRI R7 10       ; counter : number of values left to write
+SETRI R3 225      ; waitLoop set address for the P() semop on semWrite
+CLINT R4          ; P() the semaphore semWrite
 STSHM R5 R6       ; store the value in the shared memory
-SETRI R3 225      ; set address for the V() semop
-CLINT R4          ; V() the semaphore
+SETRI R3 210      ; set address for the V() semop semRead
+CLINT R4          ; V() the semaphore semRead
+ADDRG R5 R5 R2    ; increment the address in the shared mem to write the remaining values
+ADDRG R6 R6 R2    ; increment the value to be written in shared mem
+SUBRG R7 R7 R2    ; decrement counter
+JNZRI R7 -8       ; jump to write another value while counter isn't at 0
 SETRI R9 2        ; int number for exit() 
 CLINT R9          ; kernel int2 exits the process
